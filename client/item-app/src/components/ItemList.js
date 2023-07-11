202c2480a672c646c21badfbf5dbf03f431f383e
Rahul <rahulkumarsinha5316@gmail.com>
2023-07-11 11:14:40 -0700

    Filter items

--- client/item-app/src/components/ItemList.js
+++ client/item-app/src/components/ItemList.js
@@ -1,46 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { useDispatch } from 'react-redux';
-// import { selectItem } from '/Users/rahul/Desktop/CPSC-455-Summer-2023/client/item-app/src/redux/actions.js';
-
-// const ItemList = () => {
-//   const [items, setItems] = useState([]);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     axios.get('http://localhost:5001/items')
-//       .then(response => {
-//         setItems(response.data);
-//       })
-//       .catch(error => console.error(`There was an error retrieving the items: ${error}`));
-//   }, []);
-
-//   const handleDeleteItem = (id) => {
-//     axios.delete(`http://localhost:5001/items/${id}`)
-//       .then(response => {
-//         setItems(items.filter(item => item.id !== id)); // Updates the state
-//       })
-//       .catch(error => console.error(`There was an error deleting the item: ${error}`));
-//   };
-//     console.log("hello"+ items)
-//   return (
-//     <div>
-//       <h2>Item List</h2>
-//       <ul>
-//         {items.map((item) => (
-//           <li key={item.id}>
-//             <img src={item.imageURL} alt={item.name} />
-//             <span>{item.name}</span>
-//             <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ItemList;
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
@@ -48,6 +5,8 @@ import { selectItem } from '/Users/rahul/Desktop/CPSC-455-Summer-2023/client/ite
 
 const ItemList = () => {
   const [items, setItems] = useState([]);
+  const [filterKey, setFilterKey ] = useState('');
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -72,6 +31,16 @@ const ItemList = () => {
       .catch(error => console.error(`There was an error retrieving the items: ${error}`));
   };
 
+  const handleFilterItem = () =>{
+    axios.get('http://localhost:5001/api/items')
+      .then(response => {
+        setItems(response.data.filter(item => item.name.startsWith(filterKey)));
+        
+      })
+   
+
+  }
+
 
   return (
     <div>
@@ -86,9 +55,19 @@ const ItemList = () => {
               <p>Manufacturer: {item.manufacturer}</p>
             
             <button onClick={() => handleDeleteItem(item._id)}>Delete</button>
+            
           </li>
         ))}
       </ul>
+      <label>
+        Filter:
+        <input
+          type="text"
+          value={filterKey}
+          onChange={(e) => setFilterKey(e.target.value)}
+        />
+      </label>
+      <button onClick={() => handleFilterItem()}>Filter</button>
     </div>
   );
 };
